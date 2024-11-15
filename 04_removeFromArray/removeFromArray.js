cb30192067520789b775a24e2f3e8fa9c7977cfd
Bobby DeLucia <bdelucia@asu.edu>
2024-11-15 02:38:27 -0700

    04 done hash table baby

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// O(mn) because includes() is O(n)
+// const removeFromArray = function(array, ...numToDelete) {
+//   return array.filter(element => !numToDelete.includes(element))
+// };
+
+// O(m+n) because has() is O(1)
+// Hash tables are friggin sweeeeeeet
+const removeFromArray = function(array, ...numsToDelete) {
+  const removeHash = new Set(numsToDelete)
+  return array.filter(element => !removeHash.has(element))
 };
 
 // Do not edit below this line
