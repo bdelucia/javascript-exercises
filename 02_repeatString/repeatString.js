f89637bd8afdbd62de4ce451000f226416188df2
Bobby DeLucia <bdelucia@asu.edu>
2024-11-15 01:05:36 -0700

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numOfRepeat) {
+    let returnString = ""
 
-};
+    if (numOfRepeat < 0)
+        return "ERROR"
+
+    for (let i = 0; i < numOfRepeat; i++)
+        returnString += string
+
+    return returnString;
+};  
 
 // Do not edit below this line
 module.exports = repeatString;
