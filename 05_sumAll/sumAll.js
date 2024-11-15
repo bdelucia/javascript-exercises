c31bd04fb052f6a6b569b30665058c90a6dc6933
Bobby DeLucia <bdelucia@asu.edu>
2024-11-15 15:18:37 -0700

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if(startNum === endNum)
+        return startNum;
 
+    if(startNum < 0 || endNum < 0)
+        return 'ERROR'
+
+    if(!Number.isInteger(startNum) || !Number.isInteger(endNum))
+        return 'ERROR'
+    let sum = 0;
+
+    if(startNum > endNum){
+        for(let i = endNum; i <= startNum; i++) {
+            sum += i
+        }
+    }
+    for(let i = startNum; i <= endNum; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
