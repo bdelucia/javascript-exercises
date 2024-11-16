c240a6fb75702d893a90690f9d36f43a5855339b
Bobby DeLucia <bdelucia@asu.edu>
2024-11-15 17:21:37 -0700

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+    return cleanedString === cleanedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
