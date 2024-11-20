5d423ed901317d8aa306348fedda4ff103e1cd1e
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-11-20 16:34:33 -0700

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort(function(a,b) {
+        const currentYear = 2024;
 
+        const firstAge = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const secondAge = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+
+        return secondAge - firstAge;
+    })
+
+    return people[0];
 };
 
 // Do not edit below this line
